12442b4b93bd2c748bd65d064fc7b7df9c7f65ce
mahao <397874164@qq.com>
2019-06-13 14:12:26 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/app/App.java
@@ -0,0 +1,14 @@
+package com.qfedu.app;
+
+/**
+ * Created by Winchester on 2019/6/13.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hahahaha");
+    }
+
+
+
+}
